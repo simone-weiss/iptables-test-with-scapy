99073726b627e2d37d5fb894f436a45be07560c2
Simone Weiss <simone.weiss@elektrobit.com>
2024-01-29 08:20:18 +0100

    Add new firewall rules for test
    Signed-off-by: Simone Weiß <simone.weiss@elektrobit.com>

--- meta-firewalltest/recipes-filter/iptables-config/iptables-config/iptables_setup.sh
+++ meta-firewalltest/recipes-filter/iptables-config/iptables-config/iptables_setup.sh
@@ -3,8 +3,8 @@
 echo "Loading iptable firewall rules"
 
 ip6tables -A INPUT -d fe80::10 -p ipv6-icmp -i lo -m icmp6 --icmpv6-type 135/0 -j ACCEPT
-ip6tables -A OUTPUT -p tcp –m tcp –s 2001:db8::1 –d 2001:db8:2 -i lo --sport 12345 –dport 80 -m mark --mark 0x10000000/0xffffffff -m conntrack --ctstate NEW, ESTABLISHED -j ACCEPT
+ip6tables -t nat -A OUTPUT -o eth0.42 -p tcp -d 2001:db8::2 --dport 60000 -m owner ! --uid-owner mail -j REDIRECT --to-ports 60001
+ip6tables -A OUTPUT -m mark --mark 0x11110000 -m limit --limit 20/sec --limit-burst 1 -j NFLOG --nflog-prefix "found.a.fancy.packet" --nflog-group 30
 
-
-echo "print interfaces"
-ip a
+echo "Firewall:"
+ip6tables-save -c
