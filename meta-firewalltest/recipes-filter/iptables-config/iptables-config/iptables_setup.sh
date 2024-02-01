95dee994ee9ab280b833752fb49dc74d4a155936
Simone Weiß <simone.weiss@elektrobit.com>
2024-02-01 10:39:06 +0100

    Add default drop for in and output
    Signed-off-by: Simone Weiß <simone.weiss@elektrobit.com>

--- meta-firewalltest/recipes-filter/iptables-config/iptables-config/iptables_setup.sh
+++ meta-firewalltest/recipes-filter/iptables-config/iptables-config/iptables_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "Loading iptable firewall rules"
-
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
 iptables -A INPUT -p tcp -m ttl --ttl-eq 8 -m tcp --dport 1234 --tcp-flags FIN,SYN,RST,ACK SYN -j DROP
 iptables -A INPUT -s 192.168.7.0/24 -i eth0 -p tcp -m tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t nat -A OUTPUT -d 192.168.7.1/32 -o eth0 -p tcp -m tcp --dport 100 -j REDIRECT --to-ports 60001
