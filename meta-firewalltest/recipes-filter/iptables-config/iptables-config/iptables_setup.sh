2e7693ec6473c3a9e094cfd69b25e226ca212e90
Michael Estner <44174424+MEstner@users.noreply.github.com>
2024-02-01 15:49:42 +0100

    Iptables: add rule to send test packet to host

--- meta-firewalltest/recipes-filter/iptables-config/iptables-config/iptables_setup.sh
+++ meta-firewalltest/recipes-filter/iptables-config/iptables-config/iptables_setup.sh
@@ -6,6 +6,7 @@ iptables -P OUTPUT DROP
 iptables -A INPUT -p tcp -m ttl --ttl-eq 8 -m tcp --dport 1234 --tcp-flags FIN,SYN,RST,ACK SYN -j DROP
 iptables -A INPUT -s 192.168.7.0/24 -i eth0 -p tcp -m tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -t nat -A OUTPUT -d 192.168.7.1/32 -o eth0 -p tcp -m tcp --dport 100 -j REDIRECT --to-ports 60001
+iptables -A OUTPUT -o eth0 -p tcp -m tcp --dport 60001 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 echo "Firewall:"
 iptables-save -c
